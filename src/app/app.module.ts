883855b0beb0a759459d7ede55509d364ca9a610
MONTORO LOPEZ JUAN ERNESTO <JUAN.MONTORO@PRONABEC.GOB.PE>
2018-01-22 11:14:23 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
+import { HttpModule } from '@angular/http';
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ValueComponent
+],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
