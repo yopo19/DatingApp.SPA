4c36ea27683c5397fc564b2d0bb50e79619f5882
MONTORO LOPEZ JUAN ERNESTO <JUAN.MONTORO@PRONABEC.GOB.PE>
2018-01-29 16:26:42 -0500

    Members - Resolvers

---
+++ src/app/_models/User.ts
@@ -0,0 +1,17 @@
+import { Photo } from './Photo';
+
+export interface User {
+    id: number;
+    username: string;
+    knownAs: string;
+    age: number;
+    gender: string;
+    created: Date;
+    photoUrl: string;
+    city: string;
+    country: string;
+    interests?: string;
+    introduction?: string;
+    lookingFor?: string;
+    photos?: Photo[];
+}
---
+++ src/app/_resolvers/member-detail.resolver.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Resolve, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { User } from '../_models/User';
+import { UserService } from '../../_services/user.service';
+import { AlertifyService } from '../../_services/alertify.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/of';
+
+@Injectable()
+export class MemberDetailResolver implements Resolve<User> {
+    /**
+     *
+     */
+    constructor ( private userService: UserService,
+        private router: Router,
+        private alertify: AlertifyService) { }
+    
+    resolve(route: ActivatedRouteSnapshot): Observable<User> {
+        return this.userService.getUser(route.params['id'])
+        .catch(error => {
+            this.alertify.error('Error de navegación.');
+            this.router.navigate(['/members']);
+            return Observable.of(null);
+        });
+    }
+
+}
